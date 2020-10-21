a0769a5747bd9f30bae52da3daa42976097785fd
uvsq21700262 <franck.naze78@gmail.com>
2020-10-21 19:54:30 +0200

    Definitely working

--- Fraction.java
+++ Fraction.java
@@ -1,5 +1,18 @@
 public class Fraction
 {
+	private int num;
+	private int den;
+
+    public Fraction(int num,int den)
+	{
+		this.num=num;
+		this.den=den;
+	}
+	
+	public String toString()
+    {
+        return num+"/"+den;
+    }
     public static void main(String[] args)
     {
         
--- Main.java
+++ Main.java
@@ -1,5 +1,15 @@
+import static org.junit.Assert.*;
+import org.junit.Test;
 public class Main
 {
+
+	@Test
+    public void TestToString()
+    {
+        Fraction f = new Fraction (-7,2);
+        assertEquals(f.toString(), "-7 / 2");
+    }
+
 	public static void main(String[] args)
 	{	
 	    System.out.println("Simple affichage");
